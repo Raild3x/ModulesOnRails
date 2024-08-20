dcb734c0cc2d2bd752a38fef366495bdda764a02
Logan Hunt <2dloganh@gmail.com>
2024-08-20 16:43:42 -0400

    Added wally toml files

--- setup.sh
+++ setup.sh
@@ -13,22 +13,65 @@ if [ ! -d "$SRC_DIR" ]; then
     exit 1
 fi
 
-for dir in "$SRC_DIR"/*/ ; do
+for DIR in "$SRC_DIR"/*/ ; do
     # Check if it's a directory
-    if [ ! -d "$dir" ]; then
+    if [ ! -d "$DIR" ]; then
       # If not a directory, skip to the next iteration
       continue
     fi
 
-    echo "Parsing directory: $dir"
-    RAW_NAME="$(basename "$dir")"
+    
+    RAW_NAME="$(basename "$DIR")"
+    echo "Parsing directory: $RAW_NAME"
 
-    # Find and remove everything in the src directory except the RailUtil directory
-    echo "Cleaning up directory $dir..."
-    if ! find "$dir" -mindepth 1 -maxdepth 1 ! -path "$dir"/"$RAW_NAME" -exec rm -rf {} +; then
-        echo "Error: Failed to remove files from $dir."
+    # Define an array of filenames to ignore
+    IGNORE_LIST=("$RAW_NAME" "default.project.json")
+
+    # Construct the find command with multiple -name options for ignoring files
+    IGNORE_PATHS=""
+    for IGNORE_FILE in "${IGNORE_LIST[@]}"; do
+        IGNORE_PATHS+=" ! -name \"$IGNORE_FILE\""
+    done
+
+    # Combine the ignore paths with the find command
+    COMMAND="find \"$DIR\" -mindepth 1 -maxdepth 1 $IGNORE_PATHS -exec rm -rf {} +"
+
+    # Evaluate and execute the command
+    if ! eval "$COMMAND"; then
+        echo "Error: Failed to remove files from $DIR."
         exit 1
     fi
+
+    cd "$dir""$RAW_NAME"
+    # Install the Wally Packages
+    echo "Installing Wally Package Dependencies..."
+    if ! wally install; then
+        echo "Error: Failed to install Wally packages."
+        exit 1
+    fi
+
+    cd ..
+
+    echo "Moving Packages directory up one level..."
+    if ! mv "$RAW_NAME"/Packages ./; then
+        echo "Error: Failed to move Packages directory."
+        exit 1
+    fi
+
+    # Ensure a sourcemap exists
+    echo "Generating sourcemap..."
+    if ! rojo sourcemap default.project.json -o sourcemap.json; then
+        echo "Error: Failed to generate sourcemap."
+        exit 1
+    fi
+
+    # Generate the Wally Package Types
+    echo "Generating Wally Package Types..."
+    if ! wally-package-types --sourcemap sourcemap.json src/Packages/; then
+        echo "Error: Failed to generate Wally package types."
+        exit 1
+    fi
+
 done
 
 echo "Setup complete!"
