1264f376e97b7d260e58f51292e102667730ad9c
Logan Hunt <2dloganh@gmail.com>
2024-09-17 19:15:06 -0400

    Updates for Cmdr and BaseComponent

--- lib/cmdrhandler/src/Server/BuiltInCommands/Debug/listPermissionGroups.lua
+++ lib/cmdrhandler/src/Server/BuiltInCommands/Debug/listPermissionGroups.lua
@@ -3,9 +3,6 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local Players = game:GetService("Players")
 
 --// Imports //--
-local Import = require(ReplicatedStorage.Orion.Import)
-local CmdrTypes ---@module CmdrTypes
-
 local Command = {
     Name = "listPermissionGroups";
     Description = "Prints the permission groups of a player";
@@ -28,11 +25,11 @@ local Command = {
     -- end,
 
     ServerRun = function(context, player: Player)
-        local CmdrService = Import("CmdrService")
+        local CmdrService = require(ReplicatedStorage:FindFirstChild("CmdrHandler", true) :: any).Server
         player = player or context.Executor
         --context:SendEvent(context.Executor, "PermissionGroupList", CmdrService:GetPermissions(player))
         return "Permission Groups: " .. table.concat(CmdrService:GetPermissions(player), ", ")
     end,
-} :: CmdrTypes.CommandModuleData<any>
+}
 
 return Command
