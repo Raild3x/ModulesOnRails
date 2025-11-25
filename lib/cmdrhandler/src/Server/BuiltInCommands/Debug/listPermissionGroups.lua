52b8671d9fcc0a8874f4da9fc93780ddf87b44ec
Logan Hunt <2dloganh@gmail.com>
2025-11-25 13:57:02 -0500

    Refactor permissions logic into PermissionsHandler
    Extracted all permission-related logic into a new Shared/PermissionsHandler.luau module, used by both client and server. Updated CmdrClient and CmdrServer to use PermissionsHandler for permission checks, group/rank/role permission management, and syncing permission data. Improved type definitions in CmdrTypes.luau, added new utility methods for command and permission queries, and updated built-in commands to use the new handler.

--- lib/cmdrhandler/src/Server/BuiltInCommands/Debug/listPermissionGroups.lua
+++ lib/cmdrhandler/src/Server/BuiltInCommands/Debug/listPermissionGroups.lua
@@ -4,32 +4,33 @@ local Players = game:GetService("Players")
 
 --// Imports //--
 local Command = {
-    Name = "listPermissionGroups";
-    Description = "Prints the permission groups of a player";
-    Group = "Help";
-    Args = {
-        {
-            Type = "player";
-            Name = "Player";
-            Description = "Lists the permission groups";
-            Default = Players.LocalPlayer;
-            Optional = true;
-        },
-    };
+	Name = "listPermissionGroups",
+	Description = "Prints the permission groups of a player",
+	Group = "Help",
+	Args = {
+		{
+			Type = "player",
+			Name = "Player",
+			Description = "Lists the permission groups",
+			Default = Players.LocalPlayer,
+			Optional = true,
+		},
+	},
 
-    -- ClientRun = function(context)
-    --     context.Cmdr:HandleEvent("PermissionGroupList", function(list)
-    --         context:Reply("Permission Groups: " .. table.concat(list, ", "))
-    --         print("Permission Groups: " .. table.concat(list, ", "))
-    --     end)
-    -- end,
+	-- ClientRun = function(context)
+	--     context.Cmdr:HandleEvent("PermissionGroupList", function(list)
+	--         context:Reply("Permission Groups: " .. table.concat(list, ", "))
+	--         print("Permission Groups: " .. table.concat(list, ", "))
+	--     end)
+	-- end,
 
-    ServerRun = function(context, player: Player)
-        local CmdrService = require(ReplicatedStorage:FindFirstChild("CmdrHandler", true) :: any).Server
-        player = player or context.Executor
-        --context:SendEvent(context.Executor, "PermissionGroupList", CmdrService:GetPermissions(player))
-        return "Permission Groups: " .. table.concat(CmdrService:GetPermissions(player), ", ")
-    end,
+	ServerRun = function(context, player: Player)
+		local CmdrService = require(ReplicatedStorage:FindFirstChild("CmdrHandler", true) :: any).Server
+		player = player or context.Executor
+		--context:SendEvent(context.Executor, "PermissionGroupList", CmdrService:GetPermissions(player))
+		return "Permission Groups: "
+			.. table.concat(CmdrService.PermissionsHandler:GetPlayerPermissionGroups(player), ", ")
+	end,
 }
 
 return Command
